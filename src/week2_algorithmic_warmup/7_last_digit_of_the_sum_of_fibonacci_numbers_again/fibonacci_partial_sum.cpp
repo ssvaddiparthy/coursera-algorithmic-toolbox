f2fd015b90ae9ef1b134dbc0fa8737b1e9c12354
vaddipar <vaddipar@adobe.com>
2020-08-03 00:17:17 +0530

    all problems of week2 passed!

--- src/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ src/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,8 +21,45 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+int get_pisano(long long m){
+    if (m == 0 || m == 1){
+        return m;
+    }
+
+    int a = 0,  b = 1, fib = a+b;
+    for (int i = 0; i < m*m; ++i) {
+        fib = (a+b)%m;
+        a = b;
+        b = fib;
+        if ( a % m == 0 && b % m == 1)
+            return i+1;
+
+    }
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    int pisano_len = get_pisano(m);
+    n = n % pisano_len;
+
+    if (n == 0 || n == 1){
+        return n;
+    }
+
+    long a = 0,  b = 1, fib = a+b;
+    for (int i = 2; i <= n; ++i) {
+        fib = (a+b)%m;
+        a = b;
+        b = fib;
+    }
+    return fib%m;
+}
+
+long long get_fibonacci_partial_sum_fast(long long from, long long to) {
+    return (get_fibonacci_huge_fast(to+2, 10) + 10 - get_fibonacci_huge_fast(from+1, 10)) %10;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
 }
--- src/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ src/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
@@ -18,8 +20,47 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+int get_pisano(long long m){
+    if (m == 0 || m == 1){
+        return m;
+    }
+
+    int a = 0,  b = 1, fib = a+b;
+    for (int i = 0; i < m*m; ++i) {
+        fib = (a+b)%m;
+        a = b;
+        b = fib;
+        if ( a % m == 0 && b % m == 1)
+            return i+1;
+
+    }
+}
+
+int fibonacci_sum_squares_fast(long long n) {
+    int a = 0,  b = 1, fib = a+b;
+    std::vector<int> pisano_series;
+
+    pisano_series.push_back(0);
+    pisano_series.push_back(1);
+    for (int i = 2; i < 60; ++i) {
+        fib = (a + b) % 10;
+        a = b;
+        b = fib;
+        pisano_series.push_back((fib*fib)%10);
+    }
+
+    int r = n%60;
+    int rem_sum = 0;
+    for (int i = 0; i < 60; ++i) {
+        if (i <= r){
+            rem_sum += pisano_series.at(i);
+        }
+    }
+    return (rem_sum) % 10;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_fast(n);
 }
