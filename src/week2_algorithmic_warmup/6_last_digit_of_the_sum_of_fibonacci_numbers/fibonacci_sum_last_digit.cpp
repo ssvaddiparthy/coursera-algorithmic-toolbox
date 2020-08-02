4dea3eb7d7f371585b098c40e483a7efe3816db4
vaddipar <vaddipar@adobe.com>
2020-08-02 16:25:07 +0530

    added last digit of fibonacci number for large numbers

--- src/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ src/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -18,8 +18,41 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_sum_fast(long long n) {
+    if ( n == 0 || n == 1){
+        return n;
+    }
+
+    int pisano_10 = 60;
+    long long divider = 0;
+    long long end = n;
+    if (n > pisano_10){
+        divider = n/pisano_10;
+        end = end % pisano_10;
+    }
+    if ( end == 0 || end == 1){
+        return (int) end;
+    }
+
+    int a = 0, b = 1, res = 1;
+    int sum_till_60 = 1, remaining_sum = 1;
+    for (int i = 2; i <= 60; ++i) {
+        if (i > n and n < 60)
+            return remaining_sum;
+        res = (a+b)%10;
+        a = b;
+        b = res;
+
+        if (i <= end)
+            remaining_sum = (remaining_sum+res)%10;
+        sum_till_60  = (sum_till_60+res)%10;
+    }
+
+    return (int)(((divider*sum_till_60)%10) + remaining_sum)%10;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
