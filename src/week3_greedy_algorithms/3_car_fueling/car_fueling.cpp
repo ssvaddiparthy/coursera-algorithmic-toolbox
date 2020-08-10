34e44f7fc28079d118db890d1669ced82b965785
vaddipar <vaddipar@adobe.com>
2020-08-10 12:39:56 +0530

    car fueling

--- src/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ src/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -7,8 +7,19 @@ using std::vector;
 using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+    int fuel_left = tank, refuel_count = 0;
+
+    for (int i = 0; i < stops.size(); i++) {
+        if (fuel_left - (stops[i+1] - stops[i]) < 0) {
+            refuel_count ++;
+            fuel_left = tank;
+            if (tank < (stops[i+1] - stops[i]))
+                return -1;
+        }
+        fuel_left -= (stops[i+1] - stops[i]);
+    }
+
+    return refuel_count;
 }
 
 
@@ -20,10 +31,15 @@ int main() {
     int n = 0;
     cin >> n;
 
-    vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
-        cin >> stops.at(i);
+    vector<int> stops;
+    stops.push_back(0);
+    for (size_t i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        stops.push_back(temp);
+    }
 
+    stops.push_back(d);
     cout << compute_min_refills(d, m, stops) << "\n";
 
     return 0;
