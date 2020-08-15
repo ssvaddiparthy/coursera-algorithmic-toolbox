15b2230684544c42a7debba9931603d537c1e4a4
vaddipar <vaddipar@adobe.com>
2020-08-15 13:04:34 +0530

    add the questions here itself

--- src/week3_greedy_algorithms/1_money_change/change.cpp
+++ src/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
 int get_change(int m) {
-  //write your code here
-  return n;
+    if ( m <= 0)
+        return m;
+
+    int n = 0;
+
+    if (m >= 10){
+        n += m/10;
+        m %= 10;
+    }
+
+    if (m >= 5) {
+        n += m/5;
+        m %= 5;
+    }
+
+    return n+m;
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    int m;
+    std::cin >> m;
+    std::cout << get_change(m) << '\n';
 }
